043c31e4d2045b0a11b4d6160c18e21a40254597
Programulity <venelin.spiridonov7@gmail.com>
2023-06-27 12:45:25 +0300

    bigger font

--- src/script.js
+++ src/script.js
@@ -1,10 +1,4 @@
 import * as THREE from "three";
-import * as dat from "lil-gui";
-
-/**
- * Debug
- */
-
 
 const parameters = {
   materialColor: "#e59494",
@@ -13,10 +7,6 @@ const cursor = {
   x: 0,
   y: 0,
 };
-//const gui = new dat.GUI();
-// gui.addColor(parameters, "materialColor").onChange(() => {
-//   material.color.set(parameters.materialColor);
-// });
 
 const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
@@ -29,9 +19,7 @@ const material = new THREE.MeshToonMaterial({
   wireframe: true
 });
 const sectionMeshes = [];
-/**
- * Sizes
- */
+
 const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
@@ -55,10 +43,7 @@ window.addEventListener("mousemove", (event) => {
   cursor.x = event.clientX / sizes.width - 0.5;
   cursor.y = event.clientY / sizes.height - 0.5;
 });
-/**
- * Camera
- */
-// Base camera
+
 const cameraGroup = new THREE.Group();
 scene.add(cameraGroup);
 const camera = new THREE.PerspectiveCamera(
@@ -70,9 +55,6 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.z = 6;
 cameraGroup.add(camera);
 
-/**
- * Renderer
- */
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
   alpha: true,
@@ -80,9 +62,6 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-/**
- * Animate
- */
 const clock = new THREE.Clock();
 let previousTime = 0;
 
@@ -97,47 +76,25 @@ const tick = () => {
   cameraGroup.position.y += (paralaxY - cameraGroup.position.y) * 2 * deltaTime;
   for (const mesh of sectionMeshes) {
     mesh.rotation.x = elapsedTime * 0.1;
-    mesh.rotation.x = elapsedTime * 0.12;
+    mesh.rotation.y = elapsedTime * 0.12;
   }
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
 const objectsDistance = 4;
 let scrollY = window.scrollY;
-let currentSection = 0
-
-window.addEventListener("scroll", () => {
-  scrollY = window.scrollY;
-  const newSection = Math.round(scrollY / sizes.height)
-  if(newSection != currentSection) {
-    currentSection = newSection
-  }
-  
-});
 
 const fillCanvas = () => {
   const mesh1 = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
+    new THREE.TorusKnotGeometry(1, 1.35, 100, 16),
     material,
-    
   );
-  const mesh2 = new THREE.Mesh(new THREE.ConeGeometry(1, 2, 32), material);
-  const mesh3 = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(0.8, 0.35, 100, 16),
-    material
-  );
-  mesh1.position.y = -objectsDistance * 0;
-  mesh2.position.y = -objectsDistance * 1;
-  mesh3.position.y = -objectsDistance * 2;
-
-  mesh1.position.x = 1.5;
-  mesh2.position.x = -2;
-  mesh3.position.x = 2;
-
-  sectionMeshes.push(mesh1, mesh2, mesh3);
-  scene.add(mesh1, mesh2, mesh3);
+  
+  //mesh1.position.x = 2;
+  sectionMeshes.push(mesh1);
+  scene.add(mesh1);
   const directionalLight = new THREE.DirectionalLight("white", 1);
-  directionalLight.position.set(1, 1, 0);
+  directionalLight.position.set(1, 1, 1);
   scene.add(directionalLight);
 };
 
