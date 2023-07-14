f26c164ee26070151543711cc4e74cbe1b748493
Programulity <venelin.spiridonov7@gmail.com>
2023-07-15 01:25:52 +0300

    zoom circles

--- src/Experience/Utils/RayCaster.js
+++ src/Experience/Utils/RayCaster.js
@@ -14,20 +14,24 @@ export default class SeeitRayCaster {
       this.experience.mouse,
       this.experience.camera.instance
     );
+    
 
     if (!this.experience.raycaster.objectsToTest) return;
+
+
     const intersects = this.seeitraycaster.intersectObjects(
       this.experience.raycaster.objectsToTest
     );
+    
     if (intersects.length) {
       if (!currentIntersect) {
-        console.log("mouse enter");
+        intersects[0].object.scale.set(2.0,2.0,2.0)
       }
 
       currentIntersect = intersects[0];
     } else {
       if (currentIntersect) {
-        console.log("mouse leave");
+        currentIntersect.object.scale.set(1.0,1.0,1.0)
       }
 
       currentIntersect = null;
