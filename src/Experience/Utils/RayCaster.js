9563e3e0da9d4311f9dfdaa2df71a493eab50e9f
Programulity <venelin.spiridonov7@gmail.com>
2023-07-15 20:37:41 +0300

    wheel stage

--- src/Experience/Utils/RayCaster.js
+++ src/Experience/Utils/RayCaster.js
@@ -14,24 +14,22 @@ export default class SeeitRayCaster {
       this.experience.mouse,
       this.experience.camera.instance
     );
-    
 
     if (!this.experience.raycaster.objectsToTest) return;
-
 
     const intersects = this.seeitraycaster.intersectObjects(
       this.experience.raycaster.objectsToTest
     );
-    
+
     if (intersects.length) {
       if (!currentIntersect) {
-        intersects[0].object.scale.set(2.0,2.0,2.0)
+        intersects[0].object.scale.set(2.0, 2.0, 2.0);
       }
 
       currentIntersect = intersects[0];
     } else {
       if (currentIntersect) {
-        currentIntersect.object.scale.set(1.0,1.0,1.0)
+        currentIntersect.object.scale.set(1.0, 1.0, 1.0);
       }
 
       currentIntersect = null;
--- src/Experience/Utils/Wheel.js
+++ src/Experience/Utils/Wheel.js
@@ -1,143 +1,130 @@
-import normalizeWheel from 'normalize-wheel'
-import EventEmitter from './EventEmitter'
+import normalizeWheel from "normalize-wheel";
+import EventEmitter from "./EventEmitter";
 
-
-let _direction = 0
-let _wheeling = false
-let _previousValue = 0
-let _idleTimeout = null
-let _lastTriggerTimeout = null
+let _direction = 0;
+let _wheeling = false;
+let _previousValue = 0;
+let _idleTimeout = null;
+let _lastTriggerTimeout = null;
 //let _previousValueTimeout = null
-let _recentlyTriggered = false
+let _recentlyTriggered = false;
+let _stage = 0;
 
-export default class Wheel extends EventEmitter
-{
-    constructor()
-    {
-        super()
-        this.onMouseWheel = this.onMouseWheel.bind(this)
-        this.eventName = 'wheel'
-        this.preventDefault = true
+export default class Wheel extends EventEmitter {
+  constructor() {
+    super();
+    this.onMouseWheel = this.onMouseWheel.bind(this);
+    this.eventName = "wheel";
+    this.preventDefault = true;
 
-        document.addEventListener(this.eventName, this.onMouseWheel, { passive: false })
+    document.addEventListener(this.eventName, this.onMouseWheel, {
+      passive: false,
+    });
+  }
+
+  onMouseWheel(event) {
+    this.triggered = false;
+
+    if (this.preventDefault) event.preventDefault();
+
+    // Get normalized value
+    const normalized = normalizeWheel(event);
+
+    const triggeredByDirection = this.testWheelByDirection(normalized.pixelY);
+    const triggeredByIdle = this.testWheelByIdle();
+    const triggeredByIncrease = this.testWheelByIncrease(normalized.pixelY);
+
+    this.triggered =
+      triggeredByDirection || triggeredByIdle || triggeredByIncrease;
+
+    if (this.triggered && _direction) {
+      //  console.log('triggeredByDirection', triggeredByDirection)
+      //  console.log('triggeredByIdle', triggeredByIdle)
+      //  console.log('triggeredByIncrease', triggeredByIncrease)
+      //  console.log('_direction', _direction)
+
+      this.onWheel(_direction);
+      _stage += _direction;
+      if (_stage < 0) _stage = 0;
+      //console.log(_stage);
+
+      _recentlyTriggered = true;
+
+      // Clear current timeout
+      if (_lastTriggerTimeout) {
+        window.clearTimeout(_lastTriggerTimeout);
+      }
+
+      _lastTriggerTimeout = window.setTimeout(() => {
+        _recentlyTriggered = false;
+      }, 200);
+    }
+  }
+
+  testWheelByDirection(value) {
+    let result = false;
+
+    if (value) {
+      // Get direction
+      const direction = value > 0 ? 1 : value < 0 ? -1 : 0;
+
+      // Direction changed
+      if (direction !== _direction) {
+        result = true;
+      }
+
+      // Save direction
+      _direction = direction;
     }
 
-    onMouseWheel(event)
-    {
-        this.triggered = false
+    return result;
+  }
 
-        if(this.preventDefault)
-            event.preventDefault()
+  testWheelByIdle() {
+    let result = false;
 
-        // Get normalized value
-        const normalized = normalizeWheel(event)
-
-        const triggeredByDirection = this.testWheelByDirection(normalized.pixelY)
-        const triggeredByIdle = this.testWheelByIdle()
-        const triggeredByIncrease = this.testWheelByIncrease(normalized.pixelY)
-
-        this.triggered = triggeredByDirection || triggeredByIdle || triggeredByIncrease
-
-        if(this.triggered && _direction)
-        {
-             //console.log('triggeredByDirection', triggeredByDirection)
-             //console.log('triggeredByIdle', triggeredByIdle)
-             //console.log('triggeredByIncrease', triggeredByIncrease)
-
-            this.onWheel(_direction)
-
-            _recentlyTriggered = true
-
-            // Clear current timeout
-            if(_lastTriggerTimeout)
-            {
-                window.clearTimeout(_lastTriggerTimeout)
-            }
-
-            _lastTriggerTimeout = window.setTimeout(() =>
-            {
-                _recentlyTriggered = false
-            }, 200)
-        }
+    // Clear current timeout
+    if (_idleTimeout) {
+      window.clearTimeout(_idleTimeout);
     }
 
-    testWheelByDirection(value)
-    {
-        let result = false
+    // Start new timeout
+    _idleTimeout = window.setTimeout(() => {
+      _wheeling = false;
+    }, 200);
 
-        if(value)
-        {
-            // Get direction
-            const direction = value > 0 ? 1 : (value < 0 ? -1 : 0)
+    if (!_wheeling) {
+      result = true;
+    }
+    _wheeling = true;
 
-            // Direction changed
-            if(direction !== _direction)
-            {
-                result = true
-            }
+    return result;
+  }
 
-            // Save direction
-            _direction = direction
-        }
+  testWheelByIncrease(value) {
+    let result = false;
+    const sign = value > 0 ? 1 : value < 0 ? -1 : 0;
 
-        return result
+    if (!_recentlyTriggered) {
+      if (sign > 0 && value > _previousValue * 2) {
+        result = true;
+      }
+
+      if (sign < 0 && value < _previousValue * 2) {
+        result = true;
+      }
     }
 
-    testWheelByIdle()
-    {
-        let result = false
+    _previousValue = value;
 
-        // Clear current timeout
-        if(_idleTimeout)
-        {
-            window.clearTimeout(_idleTimeout)
-        }
+    return result;
+  }
 
-        // Start new timeout
-        _idleTimeout = window.setTimeout(() =>
-        {
-            _wheeling = false
-        }, 200)
+  onWheel(direction) {
+    this.trigger("wheel", [direction]);
+  }
 
-        if(!_wheeling)
-        {
-            result = true
-        }
-        _wheeling = true
-
-        return result
-    }
-
-    testWheelByIncrease(value)
-    {
-        let result = false
-        const sign = value > 0 ? 1 : (value < 0 ? -1 : 0)
-
-        if(!_recentlyTriggered)
-        {
-            if(sign > 0 && value > _previousValue * 2)
-            {
-                result = true
-            }
-
-            if(sign < 0 && value < _previousValue * 2)
-            {
-                result = true
-            }
-        }
-
-        _previousValue = value
-
-        return result
-    }
-
-    onWheel(direction)
-    {
-        this.trigger('wheel', [direction])
-    }
-
-    destructor()
-    {
-        document.removeEventListener(this.eventName, this.onMouseWheel)
-    }
+  destructor() {
+    document.removeEventListener(this.eventName, this.onMouseWheel);
+  }
 }
