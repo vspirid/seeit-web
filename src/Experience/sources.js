cbd0768aa7d44e4daf8197c41ba2936db78b5eb8
Programulity <venelin.spiridonov7@gmail.com>
2023-07-17 21:46:54 +0300

    update sources

--- src/Experience/sources.js
+++ src/Experience/sources.js
@@ -1,36 +1,8 @@
 export default [
     {
-        name: 'environmentMapTexture',
-        type: 'cubeTexture',
-        path:
-        [
-            'textures/environmentMap/px.jpg',
-            'textures/environmentMap/nx.jpg',
-            'textures/environmentMap/py.jpg',
-            'textures/environmentMap/ny.jpg',
-            'textures/environmentMap/pz.jpg',
-            'textures/environmentMap/nz.jpg'
-        ]
-    },
-    {
-        name: 'grassColorTexture',
-        type: 'texture',
-        path: 'textures/dirt/color.jpg'
-    },
-    {
-        name: 'grassNormalTexture',
-        type: 'texture',
-        path: 'textures/dirt/normal.jpg'
-    },
-    {
-        name: 'foxModel',
-        type: 'gltfModel',
-        path: 'models/Fox/glTF/Fox.gltf'
-    },
-    {
         name: 'envMap',
         type: 'rgbeModel',
-        path: 'env/mud.hdr'
+        path: 'env/sky.hdr'
     },
     {
         name: 'torusGradientTexture',
